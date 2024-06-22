bb37e3859a0833d27558f55155f703ab9a9ae614
Sergey V. Lobanov <sergey@lobanov.in>
2024-06-22 12:06:49 +0200

    ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,38 +7,47 @@ export default defineConfig({
     /* Fail the build on CI if you accidentally left test.only in the source code. */
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    //retries: process.env.CI ? 2 : 0,
+    /* Disable retries */
+    retries: 0,
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+    reporter: [
+        ['list'],
+        ['html'],
+        ['junit', { outputFile: "test-results/test-results.xml", includeProjectInTestName: true }],
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        // baseURL: 'http://127.0.0.1:3000',
+        baseURL: 'https://playwright.dev/',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-        trace: 'retain-on-first-failure',
-        screenshot: 'only-on-failure',
+        trace: process.env.CI ? 'retain-on-first-failure' : 'on',
+        screenshot: process.env.CI ? 'only-on-failure' : 'on',
         video: {
-            mode: 'retain-on-failure',
+            mode: process.env.CI ? 'retain-on-failure' : 'on',
             size: { width: 1280, height: 720 }
         },
+        /*launchOptions: {
+            slowMo: 2500,
+        },*/
     },
 
     /* Configure projects for major browsers */
     projects: [
+        /*{
+            name: 'Google Chrome',
+            use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        },*/
+
         {
-            name: 'chromium',
-            use: { ...devices['Desktop Chrome'] },
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
         },
+
 
     ],
 
-    /* Run your local dev server before starting the tests */
-    // webServer: {
-    //   command: 'npm run start',
-    //   url: 'http://127.0.0.1:3000',
-    //   reuseExistingServer: !process.env.CI,
-    // },
 });
--- tests/test1.spec.ts
+++ tests/test1.spec.ts
@@ -1,9 +1,13 @@
 import { test } from '@playwright/test';
 
-test('repro-31358', async ({ page }) => {
-    test.setTimeout(30000);
+test('repro-31358', async ({ page, browser, browserName }) => {
+    test.setTimeout(50000);
+    console.log('CI=' + process.env.CI);
+    console.log('browserName=' + browserName);
+    console.log('browser version=' + browser.version());
+
     await page.goto('https://playwright.dev/');
 
-    // Expect a title "to contain" a substring.
-    await page.getByRole('button', { name: 'non-existent button' }).click();
+    await page.waitForTimeout(30000);
+    //await page.getByRole('button', { name: 'non-existent button' }).click();
 });
